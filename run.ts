2e8119cd1225bf593f4978af4f3ea1d8e5094bde
Haugen <hej@tobiashaugen.se>
2022-11-30 10:23:41 +0100

    init setup

---
+++ run.ts
@@ -0,0 +1,14 @@
+let day: string;
+let task: string;
+
+try {
+  day = process.argv[2].split("=")[1];
+  task = process.argv[3]?.split("=")[1] || "1";
+} catch (error) {
+  throw new Error('Missing param: "day"');
+}
+
+console.log("day", day);
+console.log("task", task);
+
+export {};
