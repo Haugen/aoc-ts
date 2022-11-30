8027cfb4d5f02be72b1bc890f6bae86ebb0e1121
Haugen <hej@tobiashaugen.se>
2022-11-30 11:17:47 +0100

    including prettier in the mix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2022
+
+Starter kit plus daily puzzle solving for [Advent of Code](https://adventofcode.com/).
+
+Setup with TypeScript, ESLint and Prettier
--- src/run.ts
+++ src/run.ts
@@ -1,14 +1,14 @@
-let day: any;
+let day;
 let task;
 
 try {
-  day = process.argv[2].split("=")[1];
-  task = process.argv[3]?.split("=")[1] || "1";
+  day = process.argv[2].split('=')[1];
+  task = process.argv[3]?.split('=')[1] || '1';
 } catch (error) {
   throw new Error('Missing param: "day"');
 }
 
-console.log("day", day);
-console.log("task", task);
+console.log('day', day);
+console.log('task', task);
 
 export {};
