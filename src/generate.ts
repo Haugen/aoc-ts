6bc0489602cb0bdc49a0642b718223404c82566e
Haugen <hej@tobiashaugen.se>
2022-11-30 16:19:50 +0100

    remove unwanted console.log

--- src/generate.ts
+++ src/generate.ts
@@ -37,7 +37,6 @@ try {
 			`https://adventofcode.com/${YEAR}/day/${day}/input`,
 			{ headers: { cookie: `session=${AOC_SESSION}` } }
 		);
-		console.log(inputData.data);
 		fs.writeFileSync(path.resolve(folderPath, 'input.txt'), inputData.data);
 
 		// Copy template file.
