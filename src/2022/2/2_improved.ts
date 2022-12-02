183c26f577bfcb67a5dbbcbced7b3c33374b7ba2
Haugen <hej@tobiashaugen.se>
2022-12-02 13:58:42 +0100

    further improvements

--- src/2022/2/2_improved.ts
+++ src/2022/2/2_improved.ts
@@ -7,37 +7,22 @@ type aChoice = 'A' | 'B' | 'C';
 type bChoice = 'X' | 'Y' | 'Z';
 type gameScore = 0 | 3 | 6;
 type myScore = 1 | 2 | 3;
-type outCome = {
-	shape: bChoice;
-	score: gameScore;
-};
 
-const gameMapper: Record<aChoice, Record<bChoice, outCome>> = {
-	A: {
-		X: { shape: 'Z', score: 0 },
-		Y: { shape: 'X', score: 3 },
-		Z: { shape: 'Y', score: 6 },
-	},
-	B: {
-		X: { shape: 'X', score: 0 },
-		Y: { shape: 'Y', score: 3 },
-		Z: { shape: 'Z', score: 6 },
-	},
-	C: {
-		X: { shape: 'Y', score: 0 },
-		Y: { shape: 'Z', score: 3 },
-		Z: { shape: 'X', score: 6 },
-	},
+const gameMapper: Record<aChoice, Record<bChoice, bChoice>> = {
+	A: { X: 'Z', Y: 'X', Z: 'Y' },
+	B: { X: 'X', Y: 'Y', Z: 'Z' },
+	C: { X: 'Y', Y: 'Z', Z: 'X' },
 };
 
 const myScoreMapper: Record<bChoice, myScore> = { X: 1, Y: 2, Z: 3 };
+const gameScoreMapper: Record<bChoice, gameScore> = { X: 0, Y: 3, Z: 6 };
 
 let totalScore = 0;
 
 inputArray.forEach((line) => {
 	const [elvChoice, myChoice] = line.split(' ') as [aChoice, bChoice];
-	const score = gameMapper[elvChoice][myChoice];
-	totalScore += score.score + myScoreMapper[score.shape];
+	const myShape = gameMapper[elvChoice][myChoice];
+	totalScore += gameScoreMapper[myChoice] + myScoreMapper[myShape];
 });
 
 console.log(totalScore);
