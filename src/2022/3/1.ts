3849faa61bada31748e0ee9a24fb3d0983d9c401
Haugen <hej@tobiashaugen.se>
2022-12-03 10:21:34 +0100

    it is ok to work with strings

--- src/2022/3/1.ts
+++ src/2022/3/1.ts
@@ -8,8 +8,8 @@ const chars: string[] = [];
 let score = 0;
 
 inputArray.forEach((line) => {
-	const first = line.slice(0, line.length / 2).split('');
-	const second = line.slice(line.length / 2, line.length).split('');
+	const first = line.slice(0, line.length / 2);
+	const second = line.slice(line.length / 2, line.length);
 
 	for (const c of first) {
 		if (second.includes(c)) {
