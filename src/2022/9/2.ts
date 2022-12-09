2c225094eafa4b1ab5c7263e277b568a3e2ff8f0
Haugen <hej@tobiashaugen.se>
2022-12-09 07:59:52 +0100

    create array dynamically

--- src/2022/9/2.ts
+++ src/2022/9/2.ts
@@ -3,24 +3,12 @@ import fs from 'fs';
 const input = fs.readFileSync('input.txt', 'utf8');
 const inputArray = input.split('\n');
 
-const snake = [
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-	'0#0',
-];
-
+const snake = Array(10).fill('0#0');
 const tailPos: string[] = [];
 
-const moveSnake = (snake1, snake2, i, cmd) => {
-	let [s1x, s1y] = snake1.split('#').map((x) => +x);
-	let [s2x, s2y] = snake2.split('#').map((x) => +x);
+const moveSnake = (pos1, pos2, i, cmd) => {
+	let [s1x, s1y] = pos1.split('#').map((x) => +x);
+	let [s2x, s2y] = pos2.split('#').map((x) => +x);
 
 	if (i === 0) {
 		if (cmd === 'L') s1x--;
@@ -54,9 +42,7 @@ const moveSnake = (snake1, snake2, i, cmd) => {
 	snake[i] = [s1x, s1y].join('#');
 	snake[i + 1] = [s2x, s2y].join('#');
 
-	if (i === 8) {
-		return tailPos.push(snake[i + 1]);
-	}
+	if (i === 8) return tailPos.push(snake[i + 1]);
 
 	moveSnake(snake[i + 1], snake[i + 2], i + 1, cmd);
 };
